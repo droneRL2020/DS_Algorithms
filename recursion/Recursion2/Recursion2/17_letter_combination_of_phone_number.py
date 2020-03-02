a5f23d8673177d57af81538c7046fb20cf393b69
droneRL2020 <seoho0527@gmail.com>
2020-03-02 18:53:46 -0500

    recursion problem solved in python

---
+++ recursion/Recursion2/Recursion2/17_letter_combination_of_phone_number.py
@@ -0,0 +1,26 @@
+
+def letterCombinations(digits):
+    A = [];
+    mp = {
+        "2":"abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz",
+    }
+    def rec(digits, cur_string = ""):
+        if len(digits) == 0:
+            A.append(cur_string)
+            return
+        digit_in = digits[0]
+        digits = digits[1:]
+        for c in mp[digit_in]:
+            rec(digits, cur_string + c)
+        return A
+    return rec("23")
+
+if __name__ == "__main__":
+      print(letterCombinations("23"))
